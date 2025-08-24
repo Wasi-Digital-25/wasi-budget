32f5c1b466d243eda496bc082e4830f6cf70e332
Wasi-Digital-25 <paulcorreach@gmail.com>
2025-08-24 17:00:52 -0500

    chore(seeders): align DemoSeeder with factories (company_id/created_by)

--- database/factories/QuoteFactory.php
+++ database/factories/QuoteFactory.php
@@ -6,6 +6,7 @@ use App\Models\Quote;
 use App\Models\Company;
 use App\Models\User;
 use App\Models\Client;
+use App\Models\QuoteItem;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -19,8 +20,13 @@ class QuoteFactory extends Factory
     {
         return [
             'company_id' => Company::factory(),
-            'user_id' => User::factory(),
-            'client_id' => Client::factory(),
+            'user_id' => User::factory()->state(fn (array $attributes) => [
+                'company_id' => $attributes['company_id'],
+            ]),
+            'client_id' => Client::factory()->state(fn (array $attributes) => [
+                'company_id' => $attributes['company_id'],
+                'created_by' => $attributes['user_id'],
+            ]),
             'number' => 'Q-' . $this->faker->unique()->numerify('000000'),
             'client_name' => $this->faker->name(),
             'client_email' => $this->faker->safeEmail(),
@@ -29,5 +35,14 @@ class QuoteFactory extends Factory
             'total_cents' => 0,
             'status' => 'draft',
         ];
+    }
+
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Quote $quote) {
+            QuoteItem::factory()->count(1)->create(['quote_id' => $quote->id]);
+            $quote->refresh();
+            $quote->save();
+        });
     }
 }
