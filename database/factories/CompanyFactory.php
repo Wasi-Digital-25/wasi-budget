d90796cf515f089311d59ed6e8d72e6c4f8242c3
Wasi-Digital-25 <paulcorreach@gmail.com>
2025-08-24 18:21:29 -0500

    chore(seeders): align DemoSeeder with factories context

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Company>
+ */
+class CompanyFactory extends Factory
+{
+    protected $model = Company::class;
+
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->company();
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'plan' => 'starter',
+            'currency' => 'PEN',
+        ];
+    }
+}
