84f487cfd7fd0bdd568abd915c957cdc70f781bb
Wasi-Digital-25 <paulcorreach@gmail.com>
2025-08-24 09:40:13 -0500

    feat(routes): agregar rutas de clientes y presupuestos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\QuoteController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('clients', ClientController::class);
+    Route::resource('quotes', QuoteController::class);
 });
 
 require __DIR__.'/auth.php';
